5d5194e29552cc3afda2c948070a3574fd0c7de9
jjmmnn <jasmine6@sheridancollege.ca>
2024-01-26 22:47:15 -0500

    hi

---
+++ src/jasmine_j_ice1/Jasmine_j_ICE1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jasmine_j_ice1;
+
+/**
+ *
+ * @author jasmi
+ */
+public class Jasmine_j_ICE1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
